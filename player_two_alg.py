43bf06b2ea3874cdb4a351d4fc14c7c754d52f54
stevenkerr <smkkerr@gmail.com>
2015-12-29 17:26:42 -0800

    Main game play file

--- player_two_alg.py
+++ player_two_alg.py
@@ -1,7 +1,7 @@
 __author__ = 'stevenkerr'
 
 
-def manual_option_action_choice(mora,turn):
+def manual_option_action_choice(mora, turn):
     if mora == 'Manual':
         print "Actions remaining: ", turn.actions_remaining
         print "Available actions: ", turn.actions_available
@@ -14,7 +14,7 @@ def manual_option_action_choice(mora,turn):
 
 
 def action_choice(player_info):
-    villages_in_hand = check_for_card_type(player_info.hand,'Village')
+    villages_in_hand = check_for_card_type(player_info.hand, 'Village')
     if villages_in_hand > 0:
         action = 'Village'
     else:
@@ -22,8 +22,7 @@ def action_choice(player_info):
     return action
 
 
-def execute_action_strategy(turn,action):
-
+def execute_action_strategy(player_info, action):
     if action.name == 'Remodel':
         strategy = []
         strategy_trash = raw_input("Choose card to upgrade ")
@@ -51,48 +50,48 @@ def execute_action_strategy(turn,action):
     return strategy
 
 
-def check_for_card_type(list,looking_for):
+def check_for_card_type(list, looking_for):
     number_found = 0
-    for x in range(0,len(list)):
+    for x in range(0, len(list)):
         if list[x] == looking_for:
             number_found += 1
     return number_found
 
+
 def buy_choice(player_info):
-   smithies_for_player = check_player_for_card_type(player_info, 'Smithy')
-   villages_for_player = check_player_for_card_type(player_info, 'Village')
-   t = player_info.treasure
-   if t >= 8:
-       buy = 'Province'
+    smithies_for_player = check_player_for_card_type(player_info, 'Smithy')
+    villages_for_player = check_player_for_card_type(player_info, 'Village')
+    t = player_info.treasure
+    if t >= 8:
+        buy = 'Province'
 
-   elif t >= 5 and player_info.bank['Province'] <= 4 and player_info.bank['Duchy'] >0:
-      buy = 'Duchy'
+    elif t >= 5 and player_info.bank['Province'] <= 4 and player_info.bank['Duchy'] > 0:
+        buy = 'Duchy'
 
-   elif t >= 6:
-       buy = 'Gold'
+    elif t >= 6:
+        buy = 'Gold'
 
-   elif t>= 4 and smithies_for_player <2:
-       buy = 'Smithy'
+    elif t >= 4 and smithies_for_player < 2:
+        buy = 'Smithy'
 
-   elif t >=3 and villages_for_player < 0:
-       buy = 'Village'
+    elif t >= 3 and villages_for_player < 0:
+        buy = 'Village'
 
-   elif t >= 3:
-      buy = 'Silver'
+    elif t >= 3:
+        buy = 'Silver'
 
-   else:
-       buy = 'None'
+    else:
+        buy = 'None'
 
-   return buy
+   # print "player 1 buys", buy
+    return buy
 
 
 def check_player_for_card_type(player_info, card_type):
-   in_hand = check_for_card_type(player_info.hand, card_type)
-   in_deck = check_for_card_type(player_info.deck, card_type)
-   in_discard = check_for_card_type(player_info.discard, card_type)
-   in_played_actions = check_for_card_type(player_info.actions_played, card_type)
-   total = in_deck + in_discard + in_hand + in_played_actions
+    in_hand = check_for_card_type(player_info.hand, card_type)
+    in_deck = check_for_card_type(player_info.deck, card_type)
+    in_discard = check_for_card_type(player_info.discard, card_type)
+    in_played_actions = check_for_card_type(player_info.actions_played, card_type)
+    total = in_deck + in_discard + in_hand + in_played_actions
 
-   return total
-
-
+    return total
